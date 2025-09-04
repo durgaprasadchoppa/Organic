c60f3711fc954b83ce104ff1e9f8e86a67e3238c
durgaprasadchoppa <20072cm008@gmail.com>
2025-09-04 15:41:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,9 @@ await connectDB()
 await connectCloudinary()
 
 //allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+// const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://organic-beta.vercel.app']
+
 
 // app.post('/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 
